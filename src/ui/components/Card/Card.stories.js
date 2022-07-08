b3b9a4c0c8c3f81bf639944e5f44c18c4f10917a
Ditmar <ditmarcastro@gmail.com>
2022-07-08 10:50:39 -0400

    Revert "feat(common);add card storybook (#4)"
    This reverts commit 43a361593a7e08c35e8ee52536864f107c237318.

--- src/ui/components/Card/Card.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { CardsFather } from './CardFather';
-export default {
-    title: 'ui/components/card',
-    component: CardsFather,
-}
-const Template = args => <CardsFather  {...args}/>
-export const Card = Template.bind({});
