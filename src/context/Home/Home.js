972603ad1a6bcd5ea28d2a882512afdfcbc1ef29
Juanita Mamani Campos <103228207+mamanicamposjuanita@users.noreply.github.com>
2022-07-08 23:35:20 -0400

    feat(common): register-page[PC3-2] (#5)
    * feat(common): register-page[PC3-2]
    
    * cambios
    
    * feat(cambios2): register-page[PC3-2]

--- src/context/Home/Home.js
+++ src/context/Home/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Header from "../../ui/components/header/header";
+import React from 'react';
+import {Header} from '../../ui/components/header/header';
 
 export const Home = () => {
     return <div>
