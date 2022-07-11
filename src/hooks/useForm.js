c0ff8eed6e107281923c30abe8163ed526a2ce55
Ditmar <ditmarcastro@gmail.com>
2022-07-11 19:15:49 -0400

    Revert "Feature/pc3 2 (#8)"
    This reverts commit 72414c468729102463bdf3bf0d90a6aa36fc2d80.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,23 +1,13 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const useForm = (initForm) => {
+const useForm = (initForm) => {
     const [form, setForm] = useState(initForm);
-    const [input, setInput] = useState({
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
-    const [error, setError] = useState({
-        email:'',
-        password:'',
-        confirmPassword:''
-    });
     const handlerChangeForm = ({ target }) => {
         setForm({...form, [target.name]: target.value});
-    };
+    }
     const handlerResetForm = () => {
         setForm(initForm);
-    };
-    return [input,setInput, error,setError, form, handlerChangeForm, handlerResetForm];
+    }
+    return [form, handlerChangeForm, handlerResetForm];
 }
-//export default useForm;
+export default useForm;
